c038a8845e64addaf83453fa6acb6108dd0dfce3
Ryan Rodwell <ryanrodwell@gmail.com>
2017-04-03 19:54:58 -0400

    creating tokens

---
+++ liri.js
@@ -0,0 +1,20 @@
+var twitterStuff = require("./keys.js");
+
+var consumerKey = twitterStuff.consumer_key;
+var consumerSecret = twitterStuff.consumerSecret;
+var accessTokenKey = twitterStuff.access_token_key;
+var accessTokenSecret = twitterStuff.access_token_secret;
+
+
+//my-tweets
+
+
+
+//spotify-this-song
+
+
+
+// movie-this
+
+
+//do-what-it-says

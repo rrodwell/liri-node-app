5f6610abfff2693c384c444d5b21f8cb94febf72
Ryan Rodwell <ryanrodwell@gmail.com>
2017-04-03 23:11:30 -0400

    working on connecting twitter api and twitter liri command

--- liri.js
+++ liri.js
@@ -1,20 +1,74 @@
+var fs = require("fs");
+var Twitter = require('twitter');
 var twitterStuff = require("./keys.js");
 
-var consumerKey = twitterStuff.consumer_key;
-var consumerSecret = twitterStuff.consumerSecret;
-var accessTokenKey = twitterStuff.access_token_key;
-var accessTokenSecret = twitterStuff.access_token_secret;
+var client = new Twitter({
+consumer_key: twitterStuff.consumer_key,
+consumer_secret: twitterStuff.consumerSecret,
+access_token_key: twitterStuff.access_token_key,
+access_token_secret: twitterStuff.access_token_secret
+});
 
+//command for liri from command line
+var liriCommand = process.argv[2];
+
+//fs read file function
+function read(){
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if(err){ console.log(err); }
+  });
+}
+
+//fs append to file
+function append(){
+  fs.appendFile("random.txt", numAmount, function(err) {
+    if(err){ console.log(err); }
+  });
+}
 
 //my-tweets
+function tweeter(){
+  //queryUrl
+  var params = {screen_name: 'rrodwell85'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
 
-
+      console.log(tweets);
+    }
+  });
+}
 
 //spotify-this-song
+function spotifyThis(){
 
-
+}
 
 // movie-this
+function whatMovie(){
 
+}
 
 //do-what-it-says
+function doThis(){
+
+}
+
+
+switch (liriCommand){
+  case "my-tweets":
+  tweeter();
+  console.log("run tweeter");
+  break;
+
+  case "spotify-this-song":
+  spotifyThis();
+  break;
+
+  case "movie-this":
+  whatMovie();
+  break;
+
+  case "do-what-it-says":
+  doThis();
+  break;
+}

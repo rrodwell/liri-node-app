3addd02819d4ae5aa481c90939c6b9b512140966
Ryan Rodwell <ryanrodwell@gmail.com>
2017-04-07 20:06:15 -0400

    adding object constructors, and inquirer prompt

--- liri.js
+++ liri.js
@@ -1,16 +1,20 @@
 var fs = require("fs");
 var Twitter = require('twitter');
 var twitterStuff = require("./keys.js");
+var inquirer = require("inquirer");
+var spotify = require('spotify');
 
 var client = new Twitter({
-consumer_key: twitterStuff.consumer_key,
-consumer_secret: twitterStuff.consumerSecret,
-access_token_key: twitterStuff.access_token_key,
-access_token_secret: twitterStuff.access_token_secret
+consumer_key: twitterStuff.twitterKeys.consumer_key,
+consumer_secret: twitterStuff.twitterKeys.consumer_secret,
+access_token_key: twitterStuff.twitterKeys.access_token_key,
+access_token_secret: twitterStuff.twitterKeys.access_token_secret
 });
+
 
 //command for liri from command line
 var liriCommand = process.argv[2];
+var liriName= process.argv[3];
 
 //fs read file function
 function read(){
@@ -25,28 +29,82 @@ function append(){
     if(err){ console.log(err); }
   });
 }
+//object constructor for user prompt
+function UserPrompt(type, name, message){
+  this.type = type;
+  this.name = name;
+  this.message = message;
+}
 
-//my-tweets
-function tweeter(){
-  //queryUrl
-  var params = {screen_name: 'rrodwell85', count: 20};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
+var twitterInput = new UserPrompt("input", "name", "What is your Twitter handle?");
 
-      console.log(tweets);
+var spotifyInput = new UserPrompt("input", "name", "What is the name of the Artist?");
+
+var artistsName;
+//inquirer function
+function userInquire(userObject){
+  inquirer.prompt([
+    userObject
+  ]).then(function(handle){
+    if (liriCommand === "my-tweets"){
+    //ask user what their Twitter handle is
+      tweeter(handle.name);
+    } else if (liriCommand === "spotify-this-song"){
+      //ask user what is the name  artist
+      spotifyThis(handle.name);
     }
   });
 }
 
+//my-tweets
+function tweeter(userName){
+  //take user input then uses it to gather their tweets
+  //also works for other public twitter handles not their own
+  var params = {screen_name: userName, count: 20};
+  client.get('statuses/user_timeline', params, function(err, tweets) {
 
+    if (err) { console.log(err); }
 
+    for (var i = 0; i < tweets.length; i++) {
+      console.log("-----------------------------------------");
+      console.log(tweets[i].created_at);
+      console.log(tweets[i].text);
+      console.log("-----------------------------------------");
+    }
+  });
+}
 
 //spotify-this-song
-function spotifyThis(){
+function spotifyThis(command){
+  spotify.search({ type: 'artist OR album OR track', query: command }, function(err, data) {
+      if ( err ) {
+          console.log('Error occurred: ' + err);
+          return;
+      }
+      var spotifyData = data;
+      //if data.tracks.items.length >1
+      if (numTracks > 1){
+        userInquire(spotifyInput);
+        return spotifyData;
+      } else {
+        console.log("Album: " + data.tracks.items[0].album.name);
+        console.log("Track: " + data.tracks.items[0].name);
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song Preview: " + data.tracks.items[0].preview_url);
+      }
+        //user input find artist name
+        //data.tracks.items.artists.length
+          //data.tracks.items.album.name
+          //data.tranks.items.artists.name
+  });
+}
+
+function spotifyArtist(spotifyData){
 
 }
 
-// movie-this
+
+//movie-this
 function whatMovie(){
 
 }
@@ -59,12 +117,11 @@ function doThis(){
 
 switch (liriCommand){
   case "my-tweets":
-  tweeter();
-  console.log("run tweeter");
+  userInquire(twitterInput);
   break;
 
   case "spotify-this-song":
-  spotifyThis();
+  spotifyThis(liriName);
   break;
 
   case "movie-this":
